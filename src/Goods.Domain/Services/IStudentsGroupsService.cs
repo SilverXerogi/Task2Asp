075ef744c14c7d46a6716bfe15de0cbd930f0ed4
Максим <@NUCBOX_M5ULTRA>
2026-07-06 10:18:37 +0300

    Изменен Domain под предметную область

--- src/Goods.Domain/Services/IStudentsGroupsService.cs
+++ src/Goods.Domain/Services/IStudentsGroupsService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Goods.Tools.Types.Results;
+using Students.Domain.StudentGroup;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace Students.Domain.Services
 {
-    internal interface IStudentsGroupsService
+    public interface IStudentsGroupsService
     {
+        Task<Result>SaveStudentGroup(StudentGroupABlank studentGroupABlank);
+        Task<StudentGroupA> GetStudentGroup(Guid id);
+        Task<Page<StudentGroupA>> GetStudentsGroups(Int32 page, Int32 count);
+        Task<Result> RemoveStudentsGroup(Guid id);
     }
 }
